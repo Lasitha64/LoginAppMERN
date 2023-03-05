dc6ba2fc029b84c0099f488444c62b234d248839
Lasitha64 <dakumpitiyalasitha2@gmail.com.com>
2023-03-05 18:17:51 +0530

    Add tailwind css

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {createBrowserRouter,Router,RouterProvider} from 'react-router-dom'
+
+//routes available
+const router= createBrowserRouter([
+    {
+        path: '/',
+        element : <div>Root router</div>
+    },
+    {
+        path: '/register',
+        element : <div>Register router</div>
+    },
+])
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+        <main>
+            <RouterProvider routes={router}></RouterProvider>
+        </main>
+    );
 }
 
 export default App;
