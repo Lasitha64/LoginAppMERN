484df03fcb54a037dda862983e01b85efafaddc2
Lasitha64 <dakumpitiyalasitha2@gmail.com.com>
2023-03-17 08:40:17 +0530

    Add new components

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,45 @@
 import './App.css';
 import {createBrowserRouter,Router,RouterProvider} from 'react-router-dom'
 
+// import all components
+import Username from './components/Username';
+import Password from './components/Password';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Register from './components/Register';
+import PageNotFound from './components/PageNotFound';
+import Reset from './components/Reset';
+
+
 //routes available
 const router= createBrowserRouter([
     {
         path: '/',
-        element : <div>Root router</div>
+        element :<Username></Username>
     },
     {
         path: '/register',
-        element : <div>Register router</div>
+        element : <Register></Register>
+    },
+    {
+        path: '/password',
+        element : <Password></Password>
+    },
+    {
+        path: '/profile',
+        element : <Profile></Profile>
+    },
+    {
+        path: '/recovery',
+        element : <Recovery></Recovery>
+    },
+    {
+        path: '/reset',
+        element : <Reset></Reset>
+    },
+    {
+        path: '*',              //invalid router
+        element : <PageNotFound></PageNotFound>
     },
 ])
 
