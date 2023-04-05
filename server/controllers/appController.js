860f044ce8bbeb543be8c4d307275ffe1de9ddd4
Lasitha64 <dakumpitiyalasitha2@gmail.com.com>
2023-04-05 21:03:30 +0530

    Login API added, verify user middleware added

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -1,5 +1,33 @@
 import UserModel from "../model/User.mode.js";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import env from '../config.js';
+
+// middleware for verify user
+// If the user exists, the middleware function will call the next() function to allow the request to continue to the protected route.
+// If the user does not exist, it will return a 404 error response with the message "User not found".
+
+// This middleware function can be used in routes that require authentication,
+// such as updating a user profile or accessing sensitive user information.
+// By checking the existence of the user before allowing access to the route,
+// it helps to prevent unauthorized access to protected resources.
+
+export async function verifyUser(req, res, next) {
+    try {
+        
+        const {username} = req.method == "GET" ? req.query : req.body;
+
+        // check user existence
+        let exist = await UserModel.findOne({username});
+        if (!exist) return res.status(404).send({error: 'User not found'});
+        next();
+
+
+    } catch (error) {
+        return res.status(404).send({error:"Authentication error"})
+    }
+}
+
 
 // POST: http://localhost:8082/api/register
 // @param :{
@@ -46,12 +74,12 @@ export async function register(req, res) {
           
 
         
-        // provide email and username for the functions
+        // provide email and username for the async functions
         Promise.all([existEmail(email), existUser(username)])
             .then(() => {
                 if (password) {
 
-                    bcrypt.hash(password, 10)
+                    bcrypt.hash(password, 10)   // hash password
                     .then(hashPassword => {
 
                         const user = new UserModel({
@@ -87,7 +115,47 @@ export async function register(req, res) {
 //     password
 // }
 export async function login(req, res) {
-    res.json('login route')
+    
+    // get data from request
+    const {username, password} = req.body;
+
+    try {
+        
+        // find record in db
+        UserModel.findOne({username})
+            .then((user) =>{
+
+                // compare passwords
+                bcrypt.compare(password, user.password)
+                    .then((passwordCheck) => {
+                        if (!passwordCheck) {
+                            return res.status(400).send({error:"Don't have password"})
+                        }
+
+                        // create json web token
+                        const token = jwt.sign({
+                            userId : user._id,
+                            username : user.username
+                        }, env.JWT_SECRET,{expiresIn : '24h'});
+
+                        return res.status(200).send({
+                            msg:"Login successful",
+                            username: username,
+                            token
+                        })
+                    })
+                    .catch((error) =>{
+                        console.log(error);
+                        return res.status(400).send({error: "Password doesn't match"})
+                    })
+            })
+            .catch((error) => {
+                return res.status(500).send({error : "Username not found"});
+            })
+
+    } catch (error) {
+        return res.status(500).send({error});
+    }
 }
 
 // GET: http://localhost:8082/api/user/example123
