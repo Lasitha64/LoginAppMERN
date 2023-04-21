045fbc49b04161652e36cd72b7bff927e593fbd3
Lasitha64 <dakumpitiyalasitha2@gmail.com.com>
2023-04-21 08:37:20 +0530

    send email using node emailer

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -268,3 +268,43 @@ export async function resetPassword(req, res) {
         return res.status(500).send({ error: error.message });
     }
 }
+
+/**
+//POST: http://localhost:8082/api/registerMail
+ * 
+ * @param {
+ * "username":"example123",
+ * "userEmail":"example123@example.com",
+ * "text":"",
+ * "subject:""
+ * } 
+ * 
+ */
+export const registerMail = async (req,res) =>{
+    const {username, userEmail, text, subject} = req.body;
+
+    // body of the email
+    var email ={
+        body:{
+            name: username,
+            intro:text || "Welcome to the login application",
+            outro: 'Need help, or have  questions? just reply we love to help.',
+        }
+    }
+
+    var emailBody = MailGenerator.generate(email);
+
+    let message = {
+        from: ENV.EMAIL,
+        to:userEmail,
+        subject: subject || "Signup Successful",
+        html:emailBody
+    }
+
+    // send email
+    transporter.sendMail(message)
+    .then(() =>{
+        return res.status(200).send({msg:"You should receive an email from us"})
+    })
+    .catch(err =>res.status(500).send({err}))
+}
