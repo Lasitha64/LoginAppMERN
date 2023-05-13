43f67103908cc38dce22879fe5421c520bb932f8
Lasitha64 <dakupitiyalasitha2@gmail.com>
2023-05-13 09:45:34 -0700

    custom hook  for easy fetching and management of data from an API in a React component

--- client/src/components/Password.js
+++ client/src/components/Password.js
@@ -5,8 +5,14 @@ import styles from "../styles/Username.module.css";
 import {Toaster} from 'react-hot-toast';
 import { useFormik } from 'formik';
 import { passwordValidate } from '../helper/validate';
+import {userAuthStore} from '../store/store'
+import useFetch from '../hooks/fetch.hook';
 
 export default function Password() {
+
+
+  const username = userAuthStore(state => state.auth);
+  const [{isLoading, apiData, serverError}] = useFetch(`/users/${username}`)
 
   //initial formik
   const formik = useFormik({
@@ -21,6 +27,9 @@ export default function Password() {
     },
   });
 
+  if(isLoading) return <h1 className='text-2xl font-bold'>Loading...</h1>;
+  if(serverError) return <h1 className='text-xl text-red-500'>{serverError.message}</h1> 
+
   return (
     <div className="container mx-auto">
 
@@ -30,7 +39,7 @@ export default function Password() {
       <div className="flex justify-center items-center h-screen">
         <div className={styles.glass}>
           <div className="title flex flex-col items-center">
-            <h4 className="text-5xl font-bold">Hello Again!</h4>
+            <h4 className="text-5xl font-bold">Hello {apiData?.firstName || apiData?.username}!</h4>
             <span className="py-4 text-xl w-2/3 text-center text-gray-5">
               Explore more by connecting with us.
             </span>
@@ -39,7 +48,7 @@ export default function Password() {
           {/* form */}
           <form action="" className="py-1" onSubmit={formik.handleSubmit}>
             <div className="profile flex justify-center py-4">
-              <img src={avatar} className={styles.profile_img} alt="avatar" />
+              <img src={apiData?.profile || avatar} className={styles.profile_img} alt="avatar" />
             </div>
 
             <div className="textbox flex flex-col items-center">
